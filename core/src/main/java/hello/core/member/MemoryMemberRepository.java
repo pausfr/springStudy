8c004a599e1d3085a295f9ab25e51c8065e09980
pausfr <lost7l@icloud.com>
2022-08-31 11:08:19 +0900

    오랜만에

--- core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository{
 
     // 실무에서는 컨쿼런트 해시맵 사용용
